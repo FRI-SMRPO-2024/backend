a60171e68a282abcf0704d1cd8ca52bc0169012a
potocnikvid <vidpotocnik0@gmail.com>
2024-03-09 21:47:36 +0100

    commented code

--- src/api/guards/jwt-guard.ts
+++ src/api/guards/jwt-guard.ts
@@ -1,36 +1,35 @@
 import { AuthService } from '../services/auth.service';
 import logger from '../../utils/winston-logger';
 import type { Request, Response, NextFunction, RequestHandler } from "express";
-import { DashboardUser } from '../models/dashboard-user.model';
 
-export const jwtGuard: RequestHandler = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        let token: string = <string>req.headers['authorization'] || '';
-        if (!token) {
-            const errorMsg = 'no authorization token'
-            logger.log('error', 'dashboardApi-jwtGuard | Error | ' + errorMsg)
-            return res.status(401).send({error: errorMsg});
-        }
-        const acessToken = token.split(" ")[1] || '';
-        const verifiedDashboardUser: DashboardUser | null = await AuthService.verifyJwtAccessToken(acessToken);
-        if (verifiedDashboardUser) {
-            req.body.dashboardUser = verifiedDashboardUser;
-            next();
-            return req;
-        } else {
-            const errorMsg = 'verifyData is null'
-            logger.log('error', 'dashboardApi-jwtGuard | Error | ' + errorMsg)
-            return res.status(401).send({error: errorMsg})
-        }
-    } catch (e) {
-        if (e instanceof Error) {
-            const errorMsg = String(e.message)
-            logger.log('error', 'dashboardApi-jwtGuard | Error | ' + errorMsg)
-            return res.status(401).send({error: errorMsg})
-        } else {   
-            const errorMsg = String(e)
-            logger.log('error', 'dashboardApi-jwtGuard | Error | ' + errorMsg)
-            return res.status(401).send({error: errorMsg})
-        }
-    }
-}
+// export const jwtGuard: RequestHandler = async (req: Request, res: Response, next: NextFunction) => {
+//     try {
+//         let token: string = <string>req.headers['authorization'] || '';
+//         if (!token) {
+//             const errorMsg = 'no authorization token'
+//             logger.log('error', 'dashboardApi-jwtGuard | Error | ' + errorMsg)
+//             return res.status(401).send({error: errorMsg});
+//         }
+//         const acessToken = token.split(" ")[1] || '';
+//         const verifiedDashboardUser: DashboardUser | null = await AuthService.verifyJwtAccessToken(acessToken);
+//         if (verifiedDashboardUser) {
+//             req.body.dashboardUser = verifiedDashboardUser;
+//             next();
+//             return req;
+//         } else {
+//             const errorMsg = 'verifyData is null'
+//             logger.log('error', 'dashboardApi-jwtGuard | Error | ' + errorMsg)
+//             return res.status(401).send({error: errorMsg})
+//         }
+//     } catch (e) {
+//         if (e instanceof Error) {
+//             const errorMsg = String(e.message)
+//             logger.log('error', 'dashboardApi-jwtGuard | Error | ' + errorMsg)
+//             return res.status(401).send({error: errorMsg})
+//         } else {   
+//             const errorMsg = String(e)
+//             logger.log('error', 'dashboardApi-jwtGuard | Error | ' + errorMsg)
+//             return res.status(401).send({error: errorMsg})
+//         }
+//     }
+// }
