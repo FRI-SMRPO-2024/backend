ba48f5cc4c18d3128b569d7e82ba348fa9981e43
mih4god3c <miha.godec22@gmail.com>
2024-03-11 19:25:56 +0100

    Added sprint endpoints (NOT TESTED YET)

---
+++ src/api/controllers/sprint.controller.ts
@@ -0,0 +1,80 @@
+import logger from '../../utils/winston-logger';
+import { Request, Response } from 'express'
+import { SprintService } from '../services/sprint.service';
+import { SprintModel } from '../models/sprint.model';
+
+export class SprintController {
+    public static async getCurrentSprint(req: Request, res: Response) {
+        try {
+            const projectId = parseInt(req.params.projectId);
+            const response = await SprintService.getCurrentSprint(projectId);
+            logger.log('info', 'api-SprintController-getCurrentSprint() | SUCCESS')
+            res.status(200).send(response);
+        } catch (e: unknown) {
+            const typedE = e as Error
+            logger.log('error', 'api-SprintController-getCurrentSprint() | Error | ' + String(typedE.message))
+            res.status(500).send({error: typedE.message});
+        }
+    }
+    public static async getSprints(req: Request, res: Response) {
+        try {
+            const projectId = parseInt(req.params.projectId);
+            const response = await SprintService.getSprints(projectId);
+            logger.log('info', 'api-SprintController-getSprints() | SUCCESS')
+            res.status(200).send(response);
+        } catch (e: unknown) {
+            const typedE = e as Error
+            logger.log('error', 'api-SprintController-getSprints() | Error | ' + String(typedE.message))
+            res.status(500).send({error: typedE.message});
+        }
+    }
+    public static async getSprint(req: Request, res: Response) {
+        try {
+            const id = parseInt(req.params.id);
+            const response = await SprintService.getSprintById(id);
+            logger.log('info', 'api-SprintController-getSprint() | SUCCESS')
+            res.status(200).send(response);
+        } catch (e: unknown) {
+            const typedE = e as Error
+            logger.log('error', 'api-SprintController-getSprint() | Error | ' + String(typedE.message))
+            res.status(500).send({error: typedE.message});
+        }
+    }
+    public static async createSprint(req: Request, res: Response) {
+        try {
+            const { velocity, project_id, start_date, end_date } = req.body;
+            const response = await SprintService.createSprint(velocity, project_id, start_date, end_date);
+            logger.log('info', 'api-SprintController-createSprint() | SUCCESS')
+            res.status(200).send(response);
+        } catch (e: unknown) {
+            const typedE = e as Error
+            logger.log('error', 'api-SprintController-createSprint() | Error | ' + String(typedE.message))
+            res.status(500).send({error: typedE.message});
+        }
+    }
+    public static async updateSprint(req: Request, res: Response) {
+        try {
+            const id = parseInt(req.params.id);
+            const { velocity, start_date, end_date } = req.body;
+            const response = await SprintService.updateSprint(id, velocity, start_date, end_date);
+            logger.log('info', 'api-SprintController-updateSprint() | SUCCESS')
+            res.status(200).send(response);
+        } catch (e: unknown) {
+            const typedE = e as Error
+            logger.log('error', 'api-SprintController-updateSprint() | Error | ' + String(typedE.message))
+            res.status(500).send({error: typedE.message});
+        }
+    }
+    public static async deleteSprint(req: Request, res: Response) {
+        try {
+            const id = parseInt(req.params.id);
+            const response = await SprintService.deleteSprint(id);
+            logger.log('info', 'api-SprintController-deleteSprint() | SUCCESS')
+            res.status(200).send(response);
+        } catch (e: unknown) {
+            const typedE = e as Error
+            logger.log('error', 'api-SprintController-deleteSprint() | Error | ' + String(typedE.message))
+            res.status(500).send({error: typedE.message});
+        }
+    }
+}
