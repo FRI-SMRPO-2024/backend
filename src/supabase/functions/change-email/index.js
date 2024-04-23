d30d2862bf5cd776a9d575b6af971d640c2272a7
mih4god3c <miha.godec22@gmail.com>
2024-04-23 08:34:24 +0200

    Added an edge function to change users email

---
+++ src/supabase/functions/change-email/index.js
@@ -0,0 +1,53 @@
+import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+
+const updateUserEmail = async (userId: string, newEmail: string, supabase:any) => {
+  const { data: user, error } = await supabase.auth.admin.updateUserById(
+    userId,
+    { email: newEmail }
+  )
+  if (error) throw error
+  
+  console.log(user)
+}
+
+serve(async (req)=>{
+    const supabase = createClient(Deno.env.get("SUPABASE_URL") ?? "", Deno.env.get("SUPABASE_ANON_KEY") ?? "", {
+        global: {
+            headers: {
+                Authorization: ("Bearer " + Deno.env.get("SUPABASE_SERVICE_ROLE_KEY")) ?? ""
+            }
+        }
+    });
+    try {
+        
+        const data = await req.json();
+        const userId = data.userId;
+        const newEmail = data.newEmail;
+
+        if (userId) {
+
+            const user = await updateUserEmail(userId, newEmail, supabase);            
+            return new Response(JSON.stringify({
+                "message": "Email changed successfully! Please logout and login again.",
+                "user": user
+            }), {
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                status: 200
+            });
+        } else {
+            throw new Error("Permission denied");
+        }
+    } catch (error) {
+        return new Response(JSON.stringify({
+            message: error.message
+        }), {
+            headers: {
+                "Content-Type": "application/json"
+            },
+            status: 400
+        });
+    }
+});
